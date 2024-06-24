3b524d426752c85af8e65b06a2fe1896b2068a53
Vincent Nyanga <vince.nyanga@hotmail.com>
2024-06-24 16:29:56 +0200

    repl

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,35 @@ use std::io::{self, Write};
 
 fn main() {
     // Uncomment this block to pass the first stage
+    wait_for_input();
+
+    loop {
+        let input = get_input();
+        let command = input.trim();
+        handle_command(command);
+        wait_for_input();
+    }
+}
+
+fn handle_command(command: &str) {
+    match command {
+        "exit" => {
+            std::process::exit(0);
+        }
+        _ => {
+            println!("{}: command not found", command);
+        }
+    }
+}
+
+fn wait_for_input(){
     print!("$ ");
     io::stdout().flush().unwrap();
+}
 
-    // Wait for user input
+fn get_input() -> String {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
-    
-    let command = input.trim();
-    println!("{}: command not found", command);
+    input
 }
